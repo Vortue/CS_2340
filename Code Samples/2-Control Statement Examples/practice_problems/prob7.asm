bd1ab79c4332f3cc42f21c2a09ac23d9219135e6
Vortue <73729541+Vortue@users.noreply.github.com>
2022-02-21 15:58:35 -0600

    Update prob7.asm
    Practice functions

--- Code Samples/2-Control Statement Examples/practice_problems/prob7.asm
+++ Code Samples/2-Control Statement Examples/practice_problems/prob7.asm
@@ -2,13 +2,31 @@
 # move the loop to a subroutine
 # while (s2[i] = s1[i] != '/0') i++; 
 	.data
-s1:	.asciiz	"hi"
+s1:	.asciiz	"hi:)"
 	.align	2
 s2:	.space	4
 
 	.text
 main:	
+	jal	moveString
+	j	exit
+	
+	
+moveString:
+	la	$t1, s1
+	la	$t2, s2
+	li	$t0, 0
+loop:
+	add	$t3, $t1, $t0
+	add	$t4, $t2, $t0
+	lw	$t5, ($t3)
+	sw	$t5, ($t4)
+	addi	$t0, $t0, 1
+	bne	$t5, $0, loop
+	jr	$ra
 
 exit:	li	$v0, 10
 	syscall
-	
+
+
+
