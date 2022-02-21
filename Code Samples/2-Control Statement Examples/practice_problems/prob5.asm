b492ccd3230bedb932b1ae0652d25a0f673ee5c7
Vortue <73729541+Vortue@users.noreply.github.com>
2022-02-21 15:41:19 -0600

    Update prob5.asm
    Practice conditionals, loops and arrays

--- Code Samples/2-Control Statement Examples/practice_problems/prob5.asm
+++ Code Samples/2-Control Statement Examples/practice_problems/prob5.asm
@@ -6,7 +6,17 @@ len:	.word	10
 
 	.text
 main:	
-
+	li	$t0, 0
+	lw	$t1, len
+	sll	$t1, $t1, 2
+	la	$t2, a
+loop:
+	add	$t3, $t0, $t2
+	lw	$t4, ($t3)
+	addi	$t4, $t4, 5
+	sw	$t4, ($t3)
+	addi	$t0, $t0, 4
+	blt	$t0, $t1, loop
 
 exit:	li	$v0, 10
 	syscall
