1befcce7b40bf5d2bbb56532cbc50610fba84ad6
Vortue <73729541+Vortue@users.noreply.github.com>
2022-02-21 15:52:11 -0600

    Update prob6.asm
    Practice loops and "pointers"

--- Code Samples/2-Control Statement Examples/practice_problems/prob6.asm
+++ Code Samples/2-Control Statement Examples/practice_problems/prob6.asm
@@ -7,8 +7,16 @@ s2:	.space	4
 
 	.text
 main:	
-
-
+	la	$t1, s1
+	la	$t2, s2
+	li	$t0, 0
+loop:
+	add	$t3, $t1, $t0
+	add	$t4, $t2, $t0
+	lw	$t5, ($t3)
+	sw	$t5, ($t4)
+	addi	$t0, $t0, 1
+	bne	$t5, $0, loop
 
 exit:	li	$v0, 10
 	syscall
